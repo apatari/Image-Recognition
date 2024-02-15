e1265348374e761598487c921021440849a3a3e5
Andy Patari <andy.patari@gmail.com>
2024-02-15 13:56:09 -0500

    add api route for image scan, hardcoded user

--- server/app.py
+++ server/app.py
@@ -10,13 +10,27 @@ from flask_restful import Resource
 from config import app, db, api
 # Add your model imports
 from models import User, Image
+from urlModel import Url
 
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+class ImageScan(Resource):
+    def post(self):
+
+        url = request.get_json()['url']
+        # get the userid from session
+
+        data = Url(url=url).getNames(1)
+
+        return {"data":data}, 200
+
+
+api.add_resource(ImageScan, '/api/image_scan')
+
+# @app.route('/')
+# def index():
+#     return '<h1>Project Server</h1>'
 
 
 if __name__ == '__main__':
