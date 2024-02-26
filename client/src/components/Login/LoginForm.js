a2b9cf4ec08334fb65667dc0fa1f1f199ad3e078
Andy Patari <andy.patari@gmail.com>
2024-02-26 09:02:03 -0500

    form elements and basic styling complete

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -7,21 +7,40 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 function LoginForm() {
     return (
         <div>
-            <Form className="m-4" >
-                <Form.Group className="m-3 form-floating" >
-                    <Form.Control
-                        type="username" 
-                        name="username" 
-                        id="username" 
-                        placeholder="Username"
-                        // value={formik.values.username}
-                        // onChange={formik.handleChange} 
-                    >
-                    </Form.Control>
-                    <Form.Label> Username </Form.Label>
+            <Col className="mx-auto" lg={5} >
+                <h3 className="m-5" >Please log in</h3>
+                <Form className="m-4" >
+                    <Form.Group className="m-3 form-floating" >
+                        <Form.Control
+                            type="username" 
+                            name="username" 
+                            id="username" 
+                            placeholder="Username"
+                            // value={formik.values.username}
+                            // onChange={formik.handleChange} 
+                        >
+                        </Form.Control>
+                        <Form.Label> Username </Form.Label>
+                    </Form.Group>
 
-                </Form.Group>
-            </Form>
+                    <Form.Group className="m-3 form-floating" >
+                        <Form.Control
+                            type="passwrod" 
+                            name="password" 
+                            id="password" 
+                            placeholder="Password"
+                            // value={formik.values.password}
+                            // onChange={formik.handleChange} 
+                        >
+                        </Form.Control>
+                        <Form.Label> Password </Form.Label>
+
+                    </Form.Group>
+
+                    <Button className="m-3" variant="primary" type="submit" >Log In</Button>
+
+                </Form>
+            </Col>
         </div>
     )
 }
