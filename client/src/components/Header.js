33a81ad7fae24fbd20f6d47832d1b0a6a9d711de
Andy Patari <andy.patari@gmail.com>
2024-02-27 11:02:19 -0500

    provide hrefs for header links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Col, Button, Navbar, Nav } from "react-bootstrap";
+import { Col, Button, Navbar, Nav, NavLink } from "react-bootstrap";
 import { UserContext } from "./App";
 
 function Header() {
@@ -22,8 +22,20 @@ function Header() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav" > 
                   <Nav>
-                    <Nav.Link>Image Library</Nav.Link>
-                    <Nav.Link>Scan an image</Nav.Link>
+                    <Nav.Link
+                      href="/"
+                      
+                    >
+                      Image Library
+                    </Nav.Link>
+
+                    <Nav.Link
+                      href="/scan"
+                      
+                    >
+                      Scan an image
+                    </Nav.Link>
+
                   </Nav>
                   
                   {Boolean(user)? <Button className="ms-auto" onClick={handleLogoutClick}>Logout</Button> : ""}
