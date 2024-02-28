bedde31df51e76221d0126761b6b286a54bc2d22
Andy Patari <andy.patari@gmail.com>
2024-02-28 09:54:40 -0500

    search feature functional

--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -6,12 +6,25 @@ import ImageCard from "./ImageCards"
 function Library() {
 
     const [images, setImages] = useState([])
+    const [searchText, setSearchText] = useState("")
+    const [imagesToDisplay, setImagesToDisplay] = useState([])
+
+    const handleSearchChange = (e) => {
+        setSearchText(e.target.value)
+    }
+
+    
 
     useEffect(() => {
         fetch('/api/images')
         .then(res => res.json())
         .then(data => setImages(data))
     }, [])
+
+    
+    const handleSearchClick = () => {
+        
+    }
 
     return (
         <div className="m-4" >
@@ -23,18 +36,20 @@ function Library() {
                 </Col>
                 <Col lg={5} >
                     <InputGroup className="mb-3 ">
-                        <Button variant="info" id="button-addon1">
-                        Search
-                        </Button>
+                        <InputGroup.Text variant="info" id="button-addon1" onClick={handleSearchClick} >
+                            Search
+                        </InputGroup.Text>
                         <Form.Control
                             aria-label="Example text with button addon"
                             aria-describedby="basic-addon1"
+                            value={searchText}
+                            onChange={handleSearchChange} 
                         />
                     </InputGroup>
                 </Col>
             </Row>
             <Row className="d-flex flex-wrap" >
-                {images.map(image => <ImageCard key={image.id} image={image} /> )}
+                {images.filter(image => image.name.toLowerCase().includes(searchText.toLowerCase())).map(image => <ImageCard key={image.id} image={image} /> )}
             </Row>
             
         </div>
