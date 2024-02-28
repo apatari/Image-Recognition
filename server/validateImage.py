efc4eb65873f72c6a529ffff40be8b5261f76b75
Andy Patari <andy.patari@gmail.com>
2024-02-28 12:40:17 -0500

    create face detection validation fn

---
+++ server/validateImage.py
@@ -0,0 +1,26 @@
+import requests
+import face_recognition
+import os
+
+class Validate:
+    def imageUrl(url):
+        try:
+            data = requests.get(url).content
+            
+            f = open('test.jpg', 'wb')
+            f.write(data)
+            f.close()
+
+            image_path = "test.jpg"
+
+            image = face_recognition.load_image_file(image_path)
+
+            os.remove('test.jpg')
+
+            faces =  len(face_recognition.face_locations(image))
+        
+            return faces
+
+
+        except:
+            return False
