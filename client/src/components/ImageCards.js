41d3f886535ef385fc05d5fc6cd37ed23da90811
Andy Patari <andy.patari@gmail.com>
2024-02-29 13:37:43 -0500

    position menu meatballs

--- client/src/components/ImageCards.js
+++ client/src/components/ImageCards.js
@@ -4,7 +4,8 @@ import { Card } from "react-bootstrap";
 function ImageCard({ image }) {
     return (
         <Card style={{ width: '12rem' }} className="m-2 p-2 bg-secondary bg-opacity-25 border-0" >
-            <Card.Img variant="top" src={image.url}  className="rounded" />
+            <Card.Img variant="top" src={image.url}  className="rounded" style={{position: "relative"}} />
+            <div className="bg-secondary bg-opacity-50 rounded fs-5"  style={{position: "absolute", top:"12px", right: "13px", width:"20px", textAlign:"center"}} >&#8942;</div>
             <Card.Body className="d-flex" >
                 <Card.Title className="mt-auto" >{image.name}</Card.Title>
             </Card.Body>
