0d4172b3ce658d7667e43f76b0ab35b2f0b20d69
Andy Patari <andy.patari@gmail.com>
2024-03-03 14:30:03 -0500

    style dropdown toggle

--- client/src/components/ImageCards.js
+++ client/src/components/ImageCards.js
@@ -6,7 +6,7 @@ function ImageCard({ image }) {
         <Card style={{ width: '12rem' }} className="m-2 p-2 bg-secondary bg-opacity-25 border-0" >
             <Card.Img variant="top" src={image.url}  className="rounded" style={{position: "relative"}} />
             <Dropdown   style={{position: "absolute", top:"12px", right: "28px", width:"20px", textAlign:"center"}} >
-                <Dropdown.Toggle variant="secondary" id="dropdown-basic" className="bg-opacity-25" >
+                <Dropdown.Toggle variant="secondary" id="dropdown-basic" style={{opacity: "45%"}} >
                     
                 </Dropdown.Toggle>
 
