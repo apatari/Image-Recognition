6f8bb0bcff0bf125e9aa13c36d885ebf2b59b7fb
Andy Patari <andy.patari@gmail.com>
2024-03-03 14:24:06 -0500

    add dropdown to cards

--- client/src/components/ImageCards.js
+++ client/src/components/ImageCards.js
@@ -5,7 +5,17 @@ function ImageCard({ image }) {
     return (
         <Card style={{ width: '12rem' }} className="m-2 p-2 bg-secondary bg-opacity-25 border-0" >
             <Card.Img variant="top" src={image.url}  className="rounded" style={{position: "relative"}} />
-            <Dropdown className="bg-secondary bg-opacity-50 rounded fs-5"  style={{position: "absolute", top:"12px", right: "13px", width:"20px", textAlign:"center"}} >&#8942;</Dropdown>
+            <Dropdown   style={{position: "absolute", top:"12px", right: "28px", width:"20px", textAlign:"center"}} >
+                <Dropdown.Toggle variant="secondary" id="dropdown-basic" className="bg-opacity-25" >
+                    
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
             <Card.Body className="d-flex" >
                 <Card.Title className="mt-auto" >{image.name}</Card.Title>
             </Card.Body>
