cb51f47816233f4a6dfe0e93093b7cbfde146e2c
Andy Patari <andy.patari@gmail.com>
2024-03-08 13:53:45 -0500

    scan form handles server errors for image urls

--- client/src/components/ScanForm.js
+++ client/src/components/ScanForm.js
@@ -1,10 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Form, Col, Row, Button } from "react-bootstrap";
+import { UserContext } from "./App";
 
 export default function ScanForm() {
 
-    const[scanUrl, setScanUrl] = useState(null)
+    const[scanUrl, setScanUrl] = useState("")
+    const [errors, setErrors] = useState([])
 
+    const [user] = useContext(UserContext)
+
+
+        const handleScanChange = (e) => {
+            setScanUrl(e.target.value)
+        }
+
+    const handleScanClick = () => {
+        setErrors([])
+        fetch('/api/image_scan', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({"url":scanUrl, "user_id": user.id})
+        })
+        .then(res => {
+            if (res.ok) {
+                console.log("good image")
+            } else {
+                res.json().then(err => setErrors(err.errors))
+            }
+        })
+    }
 
     return (
         <div className="m-3" >
@@ -15,16 +41,18 @@ export default function ScanForm() {
 
                     <Form.Group className="mb-3" >
                         <Form.Label className="fs-4">Scan an image</Form.Label>
-                        <Form.Control type="text" placeholder="Enter image URL" />
+                        <Form.Control type="text" placeholder="Enter image URL" onChange={handleScanChange} value={scanUrl} />
                         <Form.Text className="text-muted">
-                            Must be a valid image URL
+                            { (errors.length > 0)? 
+                                errors.map(error => <p className="text-danger m-2 " key={error} >{error}</p> )   
+                                : "Must be a valid image URL"}
                         </Form.Text>                                     
                     </Form.Group>
                     </Col>
                     </Row>
                 </Form>
 
-                <Button className="" >Scan</Button>
+                <Button className="" onClick={handleScanClick} >Scan</Button>
 
                     
            
