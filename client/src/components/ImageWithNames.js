c7ef3a63ea5aaca1fe6b043aa284b509dd6bdfd3
Andy Patari <andy.patari@gmail.com>
2024-03-11 08:39:55 -0400

    adjust name location

--- client/src/components/ImageWithNames.js
+++ client/src/components/ImageWithNames.js
@@ -19,8 +19,8 @@ function ImageWithNames ({ imageData, PHOTO }) {
                             style={{
                                 position:'absolute', 
                                 right:'', 
-                                left:`${item.coordinates[3] + 45}px`, 
-                                top:`${item.coordinates[2] + 130}px`, 
+                                left:`${item.coordinates[3] + 55}px`, 
+                                top:`${item.coordinates[2] + 310}px`, 
                                 color:'red'
                             }} 
                         >
